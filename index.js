307eaf4e3710927030a8ad90b61e8cbd377ff7e3
Felipe <126893383+feoliveira7@users.noreply.github.com>
2024-08-28 08:11:38 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function scrollToSobre() {
+    
+    var targetDiv = document.getElementById("divSobre");
+    
+    targetDiv.scrollIntoView({ behavior: "smooth" });
+}
+
+
+function scrollToTime() {
+    
+    var targetDiv = document.getElementById("divTime");
+    
+    targetDiv.scrollIntoView({ behavior: "smooth" });
+}
+
+
+function scrollToBaixar() {
+   
+    var targetDiv = document.getElementById("divBaixar");
+    
+    targetDiv.scrollIntoView({ behavior: "smooth" });
+}
+
+
+function linkDownload(){
+    window.location.href = 'linkMega'
+}
